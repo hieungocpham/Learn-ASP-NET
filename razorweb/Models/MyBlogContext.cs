a3ba16fc31d5186e1892623fb3dacf7a96514163
unknown <hieuphamngoc14@gmail.com>
2023-04-16 22:57:37 +0700

    idenity login register mail service

--- razorweb/Models/MyBlogContext.cs
+++ razorweb/Models/MyBlogContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace razorweb.Models
 {
-    public class MyBlogContext:DbContext
+    public class MyBlogContext: IdentityDbContext<AppUser>
     {
         public MyBlogContext(DbContextOptions<MyBlogContext> options):base(options){
 
@@ -12,6 +13,12 @@ namespace razorweb.Models
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder){
             base.OnModelCreating(modelBuilder);
+            foreach( var entityType in modelBuilder.Model.GetEntityTypes()){
+                var tableName = entityType.GetTableName();
+                if(tableName.StartsWith("AspNet")){
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
         }
         public DbSet<Article> articles {get;set;}
     }
--- razorweb/Startup.cs
+++ razorweb/Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Album.Mail;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +29,11 @@ namespace razorweb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var mailSetting =  Configuration.GetSection("MailSettings");
+            services.Configure<MailSettings>(mailSetting);
+            services.AddSingleton<IEmailSender,SendMailService>();
+
             services.AddRazorPages();
             //  Connect Database
             services.AddDbContext<MyBlogContext>(options => {
@@ -32,6 +41,42 @@ namespace razorweb
                 options.UseSqlServer(connectstring);
             });
             // End
+            // Dang ky Identity
+            /*
+            Thêm triển khai EF lưu trữ thông tin về Idetity :AddEntityFrameworkStores
+            Thêm Token Provider - nó sử dụng để phát sinh token (reset password, confirm email ...)
+            */
+            // services.AddDefaultIdentity<AppUser>().AddEntityFrameworkStores<MyBlogContext>()
+            // .AddDefaultTokenProviders();
+             services.AddIdentity<AppUser,IdentityRole>().AddEntityFrameworkStores<MyBlogContext>()
+            .AddDefaultTokenProviders();
+            // Cau hinh Identity
+            services.Configure<IdentityOptions> (options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes (5); // Khóa 5 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +97,7 @@ namespace razorweb
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
@@ -59,6 +105,8 @@ namespace razorweb
             {
                 endpoints.MapRazorPages();
             });
+            // IdentityUser user;
+            // IdentityDbContext context;
         }
     }
 }
@@ -67,5 +115,5 @@ CREATE READ Update ,delete (CRUD)
 dotnet ef migrations add [name]
 dotnet ef database update
 dotnet aspnet-codegenerator razorpage -m razorweb.Models.Article -dc razorweb.Models.MyBlogContext -outDir Pages/Blog -udl --referenceScriptLibraries
-
+dotnet aspnet-codegenerator identity -dc razorweb.Models.MyBlogContext
 */
